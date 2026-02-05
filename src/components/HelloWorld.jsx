1c45fc637c81e7529fc86bc9e5b17e19678a98cf
Charly Gibbons <cegibbons@me.com>
2026-02-05 09:04:48 -0800

    moved test to new test file

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -4,10 +4,22 @@ import { Typography } from '@mui/material';
 export default function HelloWorld() {
   return (
     <>
-    <Typography variant="h2" data-testid="hello-header" style={{ fontFamily: 'minion-pro, serif', fontSize: '22px', fontWeight: 'bold' }}>
+    <Typography variant="h2" 
+      data-testid="hello-header" 
+      style={{ 
+        fontFamily: 'minion-pro, serif', 
+        fontSize: '22px', 
+        fontWeight: 'bold' 
+        }}
+    >
       Hello, I'm Charly Gibbons.
     </Typography>
     <Typography variant="h2" style={{ fontFamily: 'minion-pro, serif', fontSize: '18px', marginTop: '10px' }}>
-      I am a Results-driven Front-End Developer skilled in building responsive, user-centric web applications using modern frameworks and design principles. Experienced in optimizing performance, enhancing accessibility, and collaborating across teams to deliver engaging digital experiences. Passionate about creating intuitive interfaces and continuously learning new technologies. </Typography></>
+      I am a Results-driven Front-End Developer skilled in building responsive, 
+      user-centric web applications using modern frameworks and design principles. 
+      Experienced in optimizing performance, enhancing accessibility, 
+      and collaborating across teams to deliver engaging digital experiences. 
+      Passionate about creating intuitive interfaces and continuously learning new technologies. 
+    </Typography></>
   );
 }
