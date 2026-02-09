90770cff760f54fb172bbdcf65af092cdeb1f448
Charly Gibbons <cegibbons@me.com>
2026-02-09 11:25:14 -0800

    adding in css fixed stylling for picture

--- src/components/CharlyPicture.jsx
+++ src/components/CharlyPicture.jsx
@@ -6,12 +6,7 @@ const CharlyPicture = () => {
     <img
       src={CharlyPic}
       alt="Charly"
-      style={{
-        width: "100%",
-        maxWidth: 300,
-        height: "auto",
-        objectFit: "cover",
-      }}
+      className="charly-picture-img"
     />
   );
 };
